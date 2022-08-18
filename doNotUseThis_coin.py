3ec00090722f52a8e42b584341c06bb5cceca49c
Codeiwomple <codeiwomple@cmcgough.co.uk>
2022-08-18 16:48:53 +0100

    Added some comments and set up the main method... still no testing... not looking forward to that

--- doNotUseThis_coin.py
+++ doNotUseThis_coin.py
@@ -1,44 +1,54 @@
 import json
 import logging
 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
+
 from setup_logger import logger
 from blockchain import Blockchain
 from block import Block
 from transaction import Transaction
+
 
 # Set logger to name of class
 logger = logging.getLogger('DoNotUseThis_coin')
 
 
 def Main():
+    # Create keys
+    print("**CREATE CRYPTO KEYS**")
+    private_key = ec.generate_private_key(ec.SECP256K1(), default_backend())
+    public_key = private_key.public_key()
+
+    # Create address from keys
+    print("**CREATE WALLET ADDRESS**")
+    myAddr = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM, format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
     # Create new blockchain
+    print("**CREATE BLOCKCHAIN**")
     myCoin = Blockchain()
 
     # Create some transactions
-    print("**CREATE TRANSACTIONS**")
-    myCoin.createTransaction(Transaction("Ben", "John", 100))
-    myCoin.createTransaction(Transaction("Jodie", "Ben", 20))
+    print("**CREATE TRANSACTION**")
+    tx1 = Transaction(myAddr, "Someone", 100)
+
+    # Sign transaction
+    print("**SIGN TRANSACTION**")
+    tx1.signTransaction(private_key)
+
+    # Add the trasaction
+    print("**ADD TRANSACTION**")
+    myCoin.addTransaction(tx1)
 
     # Mine the block
     print("**MINE THE BLOCK**")
-    myCoin.minePendingTransactions("Conor")
+    myCoin.minePendingTransactions(myAddr))
 
     # Retrieve balance
     print("**GET MY BALANCE**")
-    print(myCoin.getBalance("Conor"))
-
-    # Create some more transactions
-    print("**CREATE TRANSACTIONS**")
-    myCoin.createTransaction(Transaction("Ben", "Conor", 1000))
-    myCoin.createTransaction(Transaction("James", "Jodie", 200))
-
-    # Mine the block
-    print("**MINE THE BLOCK**")
-    myCoin.minePendingTransactions("Conor")
-
-    # Retrieve balance
-    print("**GET MY BALANCE**")
-    print(myCoin.getBalance("Conor"))
+    print(myCoin.getBalance(myAddr))
 
 
 if __name__ == "__main__":
